6b154971b94f00621dad284212d6609500c7aaeb
Adam Templeton <2567437T@student.gla.ac.uk>
2023-06-26 21:30:33 +0100

    Progress log

---
+++ avgHR.py
@@ -0,0 +1,6 @@
+import numpy as np
+import math
+
+def avgHR(numpyData):
+    print(math.ceil(np.mean(numpyData[:,1])))
+
--- openFitFile.py
+++ openFitFile.py
@@ -1,41 +1,36 @@
 from fitparse import FitFile
 import numpy as np
 import datetime as dt
+import math
 
 def openFile(fileLocation):
     openedFile = FitFile(fileLocation)
-    hrData = []
-    timeData = []
-    distanceData = []
     overallData = []
-
+    count = 0
     for record in openedFile.get_messages():
         dataVals = []
-        count = 0
+        if count < 10:
+            print(record.header)
+            print(record.mesg_type)
+
+        count += 1
         for field in record:
             if field.name == 'heart_rate':
-                hrData.append(field.value)
                 dataVals.append(field.value)
-                count += 1
+
             elif field.name == 'timestamp':
                 if type(field.value) == dt.datetime:
-                    timeData.append(field.value)
                     dataVals.append(field.value)
-                if count == 0:
-                    print(record)
-            elif field.name == 'distance':
-                distanceData.append(field.value)
-                dataVals.append(field.value)
-        if len(dataVals) == 1:
-            dataVals.append([False, False])
-            overallData.append(dataVals)
-        # elif len(dataVals) == 3:
-            #overallData.append(dataVals)
-    print(len(hrData))
-    print(len(distanceData))
-    print(len(timeData))
-    print(overallData)
 
+            elif field.name == 'distance':
+                dataVals.append(field.value)
+
+        if len(dataVals) == 3:
+            overallData.append(dataVals)
+
+    numpyData = np.array(overallData)
     openedFile.close()
+
+    return numpyData
 
 openFile("TestFiles/test1.fit")
